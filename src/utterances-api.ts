1d5831fe799e81f9f097eff801a5be0861e1bff2
hanabiiii <26705594+hanabiiii@users.noreply.github.com>
2021-04-02 13:31:47 +0700

    update api

--- src/utterances-api.ts
+++ src/utterances-api.ts
@@ -1,2 +1,2 @@
 // export const UTTERANCES_API = 'http://localhost:5000';
-export const UTTERANCES_API = 'https://dev-comments.hanabiiii.now.sh';
+export const UTTERANCES_API = 'https://dev-comments-hanabiiii.vercel.app';
