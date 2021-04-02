0320ed435e84068017c24116193e4caf649008fb
hanabiiii <26705594+hanabiiii@users.noreply.github.com>
2021-04-02 13:58:07 +0700

    update build script

--- src/theme.ts
+++ src/theme.ts
@@ -1,15 +1,17 @@
+const GH_PAGE = `https://hanabiiii.github.io/mason-dev-comments`;
+
 export function loadTheme(theme: string, origin: string) {
   return new Promise(resolve => {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.setAttribute('crossorigin', 'anonymous');
     link.onload = resolve;
-    link.href = `/stylesheets/themes/${theme}/utterances.css`;
+    link.href = GH_PAGE + `/stylesheets/themes/${theme}/utterances.css`;
     document.head.appendChild(link);
 
     addEventListener('message', event => {
       if (event.origin === origin && event.data.type === 'set-theme') {
-        link.href = `/stylesheets/themes/${event.data.theme}/utterances.css`;
+        link.href = GH_PAGE + `/stylesheets/themes/${event.data.theme}/utterances.css`;
       }
     });
   });
